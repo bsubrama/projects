41d751539674cd0952fc12ee551abeb3994f3dbd
Bharadwaj Subramanian <bharadwaj.subramanian@gmail.com>
2011-08-24 21:38:20 -0700

    Added comments and fixed whitespace.

--- regex/fa/automaton.h
+++ regex/fa/automaton.h
@@ -1,7 +1,3 @@
-/* 
- * Abstract automaton class. defines the interface exposed by all the automata
- * implemented.
- */
 #ifndef __INCLUDED_AUTOMATON_H
 #define __INCLUDED_AUTOMATON_H
 
@@ -14,17 +10,36 @@
 namespace fa {
 
 class Automaton {
+  //Abstract automaton class. defines the interface exposed by all the automata
+  //implemented.
   public:
     Automaton(const std::string& name)
     :d_name(name) {}
-    virtual void setLanguage(const std::vector<char>& language) = 0; 
+
+    virtual void setLanguage(const std::vector<char>& language) = 0;
+      // Set the language understood by the automaton to the be characters in
+      // the specified 'language'. 
     virtual bool evaluate(const std::string& input) = 0;
+      // Evaluate the specified 'input' using the current transition table and
+      // return 'true' if a final state was reached, and 'false' otherwise. 
     virtual State *getState(const std::string& name) = 0;
-    virtual void printTransitionTable() = 0;
+      // Returns a State* to the State object in the FA with the specified
+      // 'name', if present. Otherwise, returns a NULL pointer. 
+    virtual void printTransitionTable(std::ostream& os) = 0;
+      // Prints the transition table as used by the automaton to specified
+      // output stream 'os'. 
     virtual void insertState(const State **state) = 0;
+      // Insert the specified '*state' into the list of states maintained by the
+      // FA. 
   private:
-    std::string         d_name;
-    std::map<char, int> d_languageTokenMap;
+    std::string          d_name;
+      // The name of the automaton. 
+    std::map<char, int>  d_languageTokenMap;
+      // A mapping between the language tokens and the internal tokens used by
+      // the FA. Use of separate internal tokens allows for greater flexibility
+      // over using only the tokens provided in the language. 
+    std::vector<State *> d_states;
+      // A list of the states in the FA. 
 };
 }
 #endif
--- regex/fa/dfa.h
+++ regex/fa/dfa.h
@@ -1,8 +1,3 @@
-/* 
- * Contains the implementation of a discrete finite automaton.
- * Inherits from the abstract Automaton class which defines what an automaton
- * should do.
- */
 #ifndef __INCLUDED_DFA_H
 #define __INCLUDED_DFA_H
 
@@ -12,15 +7,23 @@
 namespace fa {
 
 class DiscreteFiniteAutomaton: public Automaton {
+// Contains the implementation of a discrete finite automaton.
+// Inherits from the abstract Automaton class which defines what an automaton
+// should do.
   public:
     DiscreteFiniteAutomaton(const std::string& name)
     :Automaton(name) { }
     
     virtual void setLanguage(const std::vector<char>& language);
+      // Implementation of the abstract setLanguage().
     virtual bool evaluate(const std::string& input);
+      // Implementation of the abstract evaluate(). 
     virtual State *getState(const std::string& name);
+      // Implementation of the abstract getState().
     virtual void printTransitionTable();
+      // Implementation of the abstract printTransitionTable().
     virtual void insertState(const State **state);
+      // Implementation of the abstract insertState(). 
 };
 }
 #endif
